23cfdb44056bd521b91df69a62065b69a9913840
Jean Lima <jean.jr.souza@hotmail.com>
2020-07-30 14:33:04 -0300

    Criando metodo para buscar cliente por Id no banco de dados.

--- src/main/java/JeeanLiima97/com/github/clientes/controller/ClienteController.java
+++ src/main/java/JeeanLiima97/com/github/clientes/controller/ClienteController.java
@@ -6,6 +6,7 @@ import JeeanLiima97.com.github.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -18,6 +19,12 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 
