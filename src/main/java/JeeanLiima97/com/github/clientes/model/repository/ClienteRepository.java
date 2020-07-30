1616e8fda08e62526ef1c6812e71c076cd24443e
Jean Lima <jean.jr.souza@hotmail.com>
2020-07-30 11:54:29 -0300

    Criando respositorios para Cliente e Servico

---
+++ src/main/java/JeeanLiima97/com/github/clientes/model/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package JeeanLiima97.com.github.clientes.model.repository;
+
+import JeeanLiima97.com.github.clientes.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
---
+++ src/main/java/JeeanLiima97/com/github/clientes/model/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package JeeanLiima97.com.github.clientes.model.repository;
+
+import JeeanLiima97.com.github.clientes.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
