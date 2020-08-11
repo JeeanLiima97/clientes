7798dd8b803b6bf9fb7b1bcd75a11aa02cb05c76
Jean Lima <jean.jr.souza@hotmail.com>
2020-08-11 15:15:30 -0300

    Validação e mensagem de feedback no formulario de Serviços prestados

--- src/main/java/JeeanLiima97/com/github/clientes/controller/dto/ServicoPrestadoDTO.java
+++ src/main/java/JeeanLiima97/com/github/clientes/controller/dto/ServicoPrestadoDTO.java
@@ -3,12 +3,23 @@ package JeeanLiima97.com.github.clientes.controller.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
+
+    @NotEmpty(message = "{campo.descricao.obrigatorio}")
     private String descricao;
+
+    @NotEmpty(message = "{campo.preco.obrigatorio}")
     private String preco;
+
+    @NotEmpty(message = "{campo.data.obrigatorio}")
     private String data;
+
+    @NotNull(message = "{campo.cliente.obrigatorio}")
     private Integer idCliente;
 
 }
